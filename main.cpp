b321baf3e4643e59c7abb24bb02e74d9a68b4314
DerrickAAU <80082136+DerrickAAU@users.noreply.github.com>
2021-09-08 23:13:09 -0600

    Menú funcionando
    Se corrigió el menú principal, funciona de manera adecuada

--- main.cpp
+++ main.cpp
@@ -1,42 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    do{
-        int a,b;
+int opcion;
+
+int main(){
+    while(1==1){
         cout << "   * * * * Bienvenido al menú Principal * * * *" << endl;
         cout << "    Elija una de las siguientes opciones: "<<endl;
         cout <<"1- Mantenimiento de datos"<<endl;
         cout <<"2- Reportes respectivos"<<endl;
         cout <<"3- Salir"<<endl;
-        cin >>a;
+        cin >>opcion;
 
-        switch(a)
-        {
-            case 1:
-                cout<< "Selecionó el mantenimiento de datos";
-                break;
-            case 2:
-                cout<< "Seleccionó reportes respectivos";
-                break;
-            case 3:
-                cout<< "¿Seguro que queires salir? SI---NO";
-                cout <<"1- SI"<<endl;
-                cout <<"2- NO"<<endl;
-                cin >>b;
-                if(b=1)
-                {
-                    return 0;
-                }
-                else
-                {
-                    return main();
-                }
-                break;
-        }
-    }
-    while(a != 3);
-    system("pause");
+switch(opcion)
+{
+    case 1:
+        cout<< "Selecionó el mantenimiento de datos"<<endl;
+        break;
+    case 2:
+        cout<< "Seleccionó reportes respectivos"<<endl;
+        break;
+    case 3:
+        exit(EXIT_SUCCESS);
+        break;
+
+}}
+return 0;
 
 }
